b1c764e279df0dc4842698988a5effac16796a44
amir-ny <59443829+amir-ny@users.noreply.github.com>
2021-01-16 20:51:07 +0330

    nutrition main activity

--- app/src/main/java/com/example/nutrition_system/MainActivity.java
+++ app/src/main/java/com/example/nutrition_system/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.nutrition_system;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
+
+        Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
     }
+
+    private void init(){
+        this.button=findViewById(R.id.button);
+        this.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchActivity();
+            }
+        });
+
+    }
+
+    private void launchActivity(){
+        Intent intent = new Intent(this,NutritionMainActivity.class);
+        startActivity(intent);
+    }
+
+
+
 }
