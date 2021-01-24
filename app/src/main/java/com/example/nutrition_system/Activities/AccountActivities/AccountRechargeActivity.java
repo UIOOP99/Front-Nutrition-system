a4b896ad44ecc6beeefcaabbdba863e5a820223d
Maede Rezaey <maede.rezaey@gmail.com>
2021-01-24 12:57:09 +0330

    before-connection

--- app/src/main/java/com/example/nutrition_system/Activities/AccountActivities/AccountRechargeActivity.java
+++ app/src/main/java/com/example/nutrition_system/Activities/AccountActivities/AccountRechargeActivity.java
@@ -1,6 +1,10 @@
 package com.example.nutrition_system.Activities.AccountActivities;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,9 +12,28 @@ import com.example.nutrition_system.R;
 
 public class AccountRechargeActivity extends AppCompatActivity {
 
+    Button chargeAccountBtn;
+    double price;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_recharge);
+        chargeAccountBtn.findViewById(R.id.chargeAccount);
+
+        chargeAccountBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+                String token = preferences.getString("usertoken", "");
+                chargeAccount(token,price);*/
+            }
+        });
     }
+
+
+
+    private void chargeAccount(String token, double price) {
+    }
+
+
 }
--- app/src/main/java/com/example/nutrition_system/Activities/AuthenticationActivities/LoginActivity.java
+++ app/src/main/java/com/example/nutrition_system/Activities/AuthenticationActivities/LoginActivity.java
@@ -1,8 +1,11 @@
 package com.example.nutrition_system.Activities.AuthenticationActivities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -28,6 +31,7 @@ public class LoginActivity extends SetUp {
         init();
         login();
         forgetPassword();
+
     }
 
     private void forgetPassword() {
@@ -62,8 +66,16 @@ public class LoginActivity extends SetUp {
                     startActivity(new Intent(LoginActivity.this, MainActivity.class));
                     finish();
                 }
+                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putString(getToke(username,password),"usertoken");
+                editor.apply();
             }
+
         });
+    }
+
+    private String getToke(TextInputEditText username, TextInputEditText password) {
     }
 
     private void init() {
