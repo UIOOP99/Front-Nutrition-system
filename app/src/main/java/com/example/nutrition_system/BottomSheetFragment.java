2dc430196fd90307b0ac8864b9eed1a2a41615e9
saharkh99 <saharkh9977@gmail.com>
2021-01-24 08:53:09 +0330

    add choosing food without api

---
+++ app/src/main/java/com/example/nutrition_system/BottomSheetFragment.java
@@ -0,0 +1,94 @@
+package com.example.nutrition_system;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.example.nutrition_system.model.Food;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+public class BottomSheetFragment extends BottomSheetDialogFragment {
+
+    private View view;
+    private Button selectFood;
+
+    private TextView mealType;
+    private TextView date;
+    private TextView nameFoodOne, nameFoodTwo;
+    private TextView priceFoodOne, priceFoodTwo;
+
+    private ImageView pictureFoodOne, pictureFoodTwo;
+    private Food food1, food2;
+    private String dateString;
+    private String mealTypeString;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.bottom_sheet, container, false);
+      //  getDialog().setContentView(view);
+      //  getDialog().show();
+        return view;
+    }
+
+    public void getInstance(Food food1, Food food2, String date, String mealType) {
+        this.food1 = food1;
+        this.food2 = food2;
+        dateString = date;
+        mealTypeString = mealType;
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+      //  setUpView();
+    }
+
+    private void setUpView() {
+        init();
+        nameFoodOne.setText(food1.getName());
+        nameFoodTwo.setText(food2.getName());
+        priceFoodOne.setText(food1.getPrice()+"");
+        priceFoodTwo.setText(food2.getPrice()+"");
+        mealType.setText(mealTypeString);
+        date.setText(dateString);
+
+        Glide.with(this)
+                .load(food1.getImage())
+                .apply(new RequestOptions().override(100, 100))
+                .into(pictureFoodOne);
+        Glide.with(this)
+                .load(food2.getImage())
+                .apply(new RequestOptions().override(100, 100))
+                .into(pictureFoodTwo);
+
+        //save food
+        //run
+
+    }
+
+    private void init() {
+        selectFood = view.findViewById(R.id.select_food);
+        mealType = view.findViewById(R.id.choose_food);
+        date = view.findViewById(R.id.date);
+        nameFoodOne = view.findViewById(R.id.name_Food_one);
+        nameFoodTwo = view.findViewById(R.id.name_Food_two);
+        priceFoodOne = view.findViewById(R.id.price_food_one);
+        priceFoodTwo = view.findViewById(R.id.price_food_two);
+        priceFoodOne = view.findViewById(R.id.price_food_one);
+        priceFoodTwo = view.findViewById(R.id.price_food_two);
+    }
+
+
+}
--- app/src/main/java/com/example/nutrition_system/MainActivity.java
+++ app/src/main/java/com/example/nutrition_system/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.nutrition_system;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.nutrition_system.model.Food;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView button;
+    private Food food1,food2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button=findViewById(R.id.button);
+        BottomSheetFragment dialog = new BottomSheetFragment();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BottomSheetFragment bottomFragment = new BottomSheetFragment();
+                bottomFragment.show(getSupportFragmentManager(), bottomFragment.getTag());
+                //dialog.getInstance(food1,food2,"sd","sd");
+            }
+        });
     }
+
 }
